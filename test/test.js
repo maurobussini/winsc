2863aad169b23807313ea56bbf89078ebb5a1ad6
Ben Lanning <blanning@all-mode.com>
2020-07-10 16:42:21 -0400

    Add status test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,19 @@
 const test = require('ava');
+const WinSC = require('../src/winsc.js');
 
 test('base test to get AVA off the ground', (t) => {
   
   t.pass();
+});
+
+// NOTE: this test assumes you have LanmanWorkstation
+// (Workstation) service on your windows test machine,
+// while this is a default service, it is not guaranteed
+// to be present
+test('status returns a string value for an existing service', async (t) => {
+  
+  let value = await WinSC.status('LanmanWorkstation');
+  console.log(value);
+  t.not(value, null);
+  t.regex(value, /^(RUNNING|STOPPED)$/);
 });
